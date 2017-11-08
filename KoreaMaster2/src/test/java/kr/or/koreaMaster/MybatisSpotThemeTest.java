25614aa882be6019cbc80557046efc3bcfd82081
Sujin <kwn01037@naver.com>
2017-11-08 21:24:37 +0900

    spotThemeMapper수정

--- KoreaMaster2/src/test/java/kr/or/koreaMaster/MybatisSpotThemeTest.java
+++ KoreaMaster2/src/test/java/kr/or/koreaMaster/MybatisSpotThemeTest.java
@@ -7,6 +7,7 @@ import kr.or.koreaMaster.common.db.DaoFactory;
 import kr.or.koreaMaster.common.db.MyBatisDaoFactory;
 import kr.or.koreaMaster.spotTheme.dao.SpotThemeDAO;
 import kr.or.koreaMaster.spotTheme.dao.SpotThemeDAOImpl;
+import kr.or.koreaMaster.spotTheme.domain.SpotTheme;
 
 public class MybatisSpotThemeTest {
 Logger logger = Logger.getLogger(MybatisSpotDaoTest.class);
@@ -14,8 +15,14 @@ Logger logger = Logger.getLogger(MybatisSpotDaoTest.class);
 	DaoFactory factory = new MyBatisDaoFactory();
 	SpotThemeDAO dao = (SpotThemeDAO)factory.getDao(SpotThemeDAOImpl.class);
 	
-	@Test
+//	@Test
 	public void test() {
 		logger.debug(dao.readThemeBySpot(1));
+	}
+	
+	@Test
+	public void insertTest() {
+		SpotTheme spotTheme = new SpotTheme(1, 1);
+		dao.create(spotTheme);
 	}
 }
